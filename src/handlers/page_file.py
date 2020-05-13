460848f38ae5bf124fd5cba4fe569f5488ef2ed7
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2020-05-13 08:36:49 +0000

    Get caching working on static files

--- src/handlers/page_file.py
+++ src/handlers/page_file.py
@@ -4,9 +4,11 @@
 
 from __future__ import annotations
 
+import datetime
 import http.server
 import os
 import shutil
+import time
 
 
 def page_file(self: http.server.BaseHTTPRequestHandler, filename: str, mime: str):
@@ -17,15 +19,37 @@ def page_file(self: http.server.BaseHTTPRequestHandler, filename: str, mime: str
         self.send_error(404, "File not found on disk")
         return
 
+    mod_date: int = 0
+
+    if "If-Modified-Since" in self.headers:
+        mod_date = int(
+            datetime.datetime.strptime(
+                str(self.headers["If-Modified-Since"]), "%a, %d %b %Y %H:%M:%S GMT"
+            ).timestamp()
+        )
+
     with open(filename, "rb") as contents:
         # stat(2) the file handle to get the file size.
         stat = os.fstat(contents.fileno())
+
+        self.log_message("Dates: %s vs %s", stat.st_mtime, mod_date)
+        if int(stat.st_mtime) <= mod_date:
+            self.send_response(304)
+            self.send_header("Last-Modified", self.date_time_string(int(stat.st_mtime)))
+            self.send_header("Cache-Control", "public; max-age=3600")
+            self.send_header("Expires", self.date_time_string(int(time.time() + 3600)))
+            self.end_headers()
+
+            return
 
         # Send the HTTP headers.
         self.send_response(200)
         self.send_header("Content-Type", mime)
         self.send_header("Content-Length", str(stat.st_size))
+        self.send_header("Last-Modified", self.date_time_string(int(stat.st_mtime)))
         self.send_header("Cache-Control", "public; max-age=3600")
+        self.send_header("Expires", self.date_time_string(int(time.time() + 3600)))
+
         self.end_headers()
 
         # Send the file to the client

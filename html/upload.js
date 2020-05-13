35ec29f4b5ca6616a9dbc1e52cf8499576380b10
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2020-05-13 09:21:37 +0000

    Make tooltip location dynamic

--- html/upload.js
+++ html/upload.js
@@ -20,6 +20,18 @@ list.addEventListener('change', e => {
 });
 reader.addEventListener('load', processEmpires);
 
+
+document.addEventListener('mouseover', e => {
+	if (e.target.tagName !== "BUTTON") return;
+
+	const tooltip = e.target.querySelector('span');
+
+	if (!tooltip) return;
+
+	tooltip.style.left = (Math.min(e.offsetX, 400) + 10) + "px";
+	tooltip.style.top  = (Math.max(e.offsetY, 10) + 10)  + "px";
+} );
+
 function processEmpires()
 {
 	while (list.lastChild) {
